8d5d0130a337dd90bdb24353648abc0096712093
Andy Baran <andy.baran@hashicorp.com>
2020-06-16 12:05:40 -0400

    updated module output names

--- main.tf
+++ main.tf
@@ -175,10 +175,10 @@ data "google_client_config" "provider" {}
 provider "kubernetes" {
   load_config_file = false
 
-  host  = "https://${module.gke_cluster.gke_endpoint}"
+  host  = "https://${module-gke.gke_endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+    module-gke.cluster_ca_certificate,
   )
 }
 
