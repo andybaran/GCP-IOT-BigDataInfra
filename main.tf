5829949ec4c67b367710ea747e7e268c3292347c
Andy Baran <andy.baran@hashicorp.com>
2020-06-17 17:35:12 -0400

    version fix

--- main.tf
+++ main.tf
@@ -217,7 +217,7 @@ resource "helm_release" "helm_consul" {
   name = "consul"
   repository = "https://helm.releases.hashicorp.com"
   chart = "consul"
-  version = "0.2.0"
+  version = "0.20.1"
 
   lint = true
   timeout = 600
